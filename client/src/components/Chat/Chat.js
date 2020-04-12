7452eb4f12b9b11e5f3a743a8b77731dba2bb5c2
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-04-12 19:42:11 -0300

    Preparaçao WS no client side

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client';
 
 import './Chat.css';
 
-export default function Chat() {
+let socket;
+
+export default function Chat({ location }) {
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+  const ENDPOINT = 'localhost:5000';
+
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
+
+    socket = io(ENDPOINT);
+
+    setName(name);
+    setRoom(room);
+    console.log(socket);
+  }, [ENDPOINT, location.search]);
+
   return (
     <div>
       Hello Chat
