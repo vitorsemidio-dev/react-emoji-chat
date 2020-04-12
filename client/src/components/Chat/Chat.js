d76d3e84f69eece777050a542e100c72ee3e2cc7
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-04-12 20:55:36 -0300

    Emissao de mensagens

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -9,6 +9,8 @@ let socket;
 export default function Chat({ location }) {
   const [name, setName] = useState('');
   const [room, setRoom] = useState('');
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
   const ENDPOINT = 'localhost:5000';
 
   useEffect(() => {
@@ -30,9 +32,31 @@ export default function Chat({ location }) {
     }
   }, [ENDPOINT, location.search]);
 
+  useEffect(() => {
+    socket.on('message', (message) => {
+      setMessages([...messages, message]);
+    });
+  }, [messages]);
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+
+    if (message) {
+      socket.emit('sendMessage', message, () => setMessage(''));
+    }
+  }
+
+  console.log(message, messages);
+
   return (
-    <div>
-      Hello Chat
+    <div className="outerContainer">
+      <div className="container">
+        <input 
+          value={message}
+          onChange={event => setMessage(event.target.value)}
+          onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null}
+          type="text"/>
+      </div>
     </div>
   )
 }
